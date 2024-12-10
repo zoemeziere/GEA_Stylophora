90d197524b009132333092cdf7f4263d1188f812
Zoe Meziere <71752811+zoemeziere@users.noreply.github.com>
2024-12-10 13:03:27 +1000

    Update 05_genotype_matrix.sh

--- Filtering/05_genotype_matrix.sh
+++ Filtering/05_genotype_matrix.sh
@@ -1,3 +1,17 @@
+#!/bin/bash --login
+#SBATCH --job-name="genotype_matrix"     # job name
+#SBATCH --nodes=1               # use 1 node
+#SBATCH --ntasks-per-node=1     # use 1 for single and multi core jobs
+#SBATCH --cpus-per-task=4               # number of cores per job
+#SBATCH --mem=50G                              # RAM per job given in megabytes (M), gigabytes (G), or terabytes (T)
+#SBATCH --time=02:00:00         # walltime
+#SBATCH --account=a_riginos             # group account name
+#SBATCH --partition=general             # queue name
+#SBATCH -o rgenotype_matrix_%A.o         # standard output
+#SBATCH -e genotype_matrix_%A.e             # standard error
+
+module load r/4.2.1-foss-2022a
+
 Rscript - <<EOF
 
 library(vcfR)
@@ -15,3 +29,5 @@ gen.gt.t[gen.gt.t %in% c("1/1")] <- 2
 class(gen.gt.t) <- "numeric"
 
 saveRDS(gen.gt.t, "SpisTaxon1_linked.rds")
+
+EOF
